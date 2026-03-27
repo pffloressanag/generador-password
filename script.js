1eac0f818b89ba08dfe5c74ba4e9fc518a817ca8
PFFlores <pfflores.sana@gmail.com>
2026-03-27 16:22:18 -0300

    Generador de contraseñas seguras nvo

--- script.js
+++ script.js
@@ -23,9 +23,9 @@ let longitud = document.getElementById("longitud").value;
 
 let password = "";
 
-for(let i = 0; i < longitud; i++){
+for(let i=0;i<longitud;i++){
 
-let random = Math.floor(Math.random() * caracteres.length);
+let random = Math.floor(Math.random()*caracteres.length);
 
 password += caracteres[random];
 
@@ -33,7 +33,49 @@ password += caracteres[random];
 
 document.getElementById("password").value = password;
 
+evaluarSeguridad(password);
+
 }
+
+
+function evaluarSeguridad(password){
+
+let fuerza = 0;
+
+if(password.length >= 8) fuerza++;
+if(/[A-Z]/.test(password)) fuerza++;
+if(/[0-9]/.test(password)) fuerza++;
+if(/[!@#$%^&*()_+\[\]{}<>?/]/.test(password)) fuerza++;
+
+let barra = document.getElementById("barra");
+let nivel = document.getElementById("nivel");
+
+if(fuerza <= 2){
+
+barra.style.width = "33%";
+barra.style.background = "red";
+nivel.textContent = "Seguridad: Débil";
+
+}
+
+else if(fuerza === 3){
+
+barra.style.width = "66%";
+barra.style.background = "orange";
+nivel.textContent = "Seguridad: Media";
+
+}
+
+else{
+
+barra.style.width = "100%";
+barra.style.background = "limegreen";
+nivel.textContent = "Seguridad: Fuerte";
+
+}
+
+}
+
 
 function copiarPassword(){
 
